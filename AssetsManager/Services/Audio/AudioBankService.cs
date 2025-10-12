2f4149088188e49219c149f1f7a7041ade4ea0d8
Neinndall <danielpriego94@gmail.com>
2025-10-12 02:03:57 +0200

    refact and improvement to the code

--- AssetsManager/Services/Audio/AudioBankService.cs
+++ AssetsManager/Services/Audio/AudioBankService.cs
@@ -2,12 +2,11 @@ using System.Collections.Generic;
 using System.IO;
 using System;
 using System.Linq;
-using AssetsManager.Services.Parsers;
-using AssetsManager.Views.Models;
 using LeagueToolkit.Core.Meta;
 using LeagueToolkit.Core.Meta.Properties;
 using LeagueToolkit.Hashing;
-
+using AssetsManager.Services.Parsers;
+using AssetsManager.Views.Models;
 using AssetsManager.Services.Core;
 
 namespace AssetsManager.Services.Audio
@@ -30,7 +29,7 @@ namespace AssetsManager.Services.Audio
 
         public List<AudioEventNode> ParseAudioBank(byte[] wpkData, byte[] audioBnkData, byte[] eventsData, byte[] binData, string baseName)
         {
-            var eventNameMap = GetEventsFromBin(binData, baseName);
+            var eventNameMap = BinParser.GetEventsFromBin(binData, baseName, _logService);
 
             var allWems = new Dictionary<uint, WemSoundInfo>();
             
@@ -74,7 +73,7 @@ namespace AssetsManager.Services.Audio
 
         public List<AudioEventNode> ParseSfxAudioBank(byte[] audioData, byte[] eventsData, byte[] binData, string baseName)
         {
-            var eventNameMap = GetEventsFromBin(binData, baseName);
+            var eventNameMap = BinParser.GetEventsFromBin(binData, baseName, _logService);
 
             var allWems = new Dictionary<uint, WemSoundInfo>();
             // For SFX banks, the audio data is stored inside the main .bnk file itself.
@@ -128,80 +127,6 @@ namespace AssetsManager.Services.Audio
                 eventNodes.Add(unknownNode);
                 _logService.LogDebug($"[AUDIO] Added 'Unknown' node with {unlinkedWemIds.Count} unlinked sounds.");
             }
-        }
-
-        private static uint Fnv1Hash(string input)
-        {
-            const uint offsetBasis = 2166136261;
-            const uint prime = 16777619;
-
-            uint hash = offsetBasis;
-            foreach (byte b in System.Text.Encoding.ASCII.GetBytes(input.ToLowerInvariant()))
-            {
-                hash *= prime;
-                hash ^= b;
-            }
-            return hash;
-        }
-
-        private Dictionary<uint, string> GetEventsFromBin(byte[] binData, string bankName)
-        {
-            var mapEventNames = new Dictionary<uint, string>();
-            if (binData == null || binData.Length == 0) return mapEventNames;
-
-            try
-            {
-                using var stream = new MemoryStream(binData);
-                var binTree = new BinTree(stream);
-
-                uint skinCharacterDataPropertiesHash = Fnv1a.HashLower("SkinCharacterDataProperties");
-                uint mapAudioDataPropertiesHash = Fnv1a.HashLower("MapAudioDataProperties");
-                uint skinAudioPropertiesHash = Fnv1a.HashLower("skinAudioProperties");
-                uint bankUnitsHash = Fnv1a.HashLower("bankUnits");
-                uint eventsHash = Fnv1a.HashLower("events");
-                uint nameHash = Fnv1a.HashLower("name");
-
-                foreach (var obj in binTree.Objects.Values)
-                {
-                    BinTreeContainer bankUnitsContainer = null;
-
-                    if (obj.ClassHash == skinCharacterDataPropertiesHash)
-                    {
-                        if (obj.Properties.TryGetValue(skinAudioPropertiesHash, out var skinAudioProp) && skinAudioProp is BinTreeStruct skinAudioStruct)
-                        {
-                            if (skinAudioStruct.Properties.TryGetValue(bankUnitsHash, out var bankUnitsProp) && bankUnitsProp is BinTreeContainer container)
-                            {
-                                bankUnitsContainer = container;
-                            }
-                        }
-                    }
-
-                    if (bankUnitsContainer != null)
-                    {
-                        foreach (BinTreeStruct bankUnit in bankUnitsContainer.Elements.OfType<BinTreeStruct>())
-                        {
-                            if (bankUnit.Properties.TryGetValue(nameHash, out var nameProp) && nameProp is BinTreeString nameString)
-                            {
-                                if (string.Equals(nameString.Value, bankName, StringComparison.OrdinalIgnoreCase))
-                                {
-                                    if (bankUnit.Properties.TryGetValue(eventsHash, out var eventsProp) && eventsProp is BinTreeContainer eventsContainer)
-                                    {
-                                        foreach (BinTreeString eventNameProp in eventsContainer.Elements.OfType<BinTreeString>())
-                                        {
-                                            uint eventHash = Fnv1Hash(eventNameProp.Value);
-                                            mapEventNames[eventHash] = eventNameProp.Value;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-            }
-            catch (Exception ex) { _logService.LogError(ex, "[AUDIO] Crash during BIN parsing."); }
-            _logService.Log($"[AUDIO] Finished BIN parsing. Found {mapEventNames.Count} total events for bank '{bankName}'.");
-            return mapEventNames;
         }
 
         private List<AudioEventNode> ParseEventsBank(byte[] eventsData, Dictionary<uint, string> eventNameMap, Dictionary<uint, WemSoundInfo> wemMetadata)
