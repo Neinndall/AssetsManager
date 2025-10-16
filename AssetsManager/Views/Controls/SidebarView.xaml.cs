afee40f99058f06e2ace77459299d17d8c37d53c
Neinndall <danielpriego94@gmail.com>
2025-10-16 12:12:09 +0200

    added panel navigation to expand and collapse sidebar

--- AssetsManager/Views/Controls/SidebarView.xaml.cs
+++ AssetsManager/Views/Controls/SidebarView.xaml.cs
@@ -1,38 +1,64 @@
 using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace AssetsManager.Views.Controls
 {
-  public partial class SidebarView : UserControl
-  {
-    // Propiedad para controlar si la sidebar está expandida
-    public static readonly DependencyProperty IsExpandedProperty =
-        DependencyProperty.Register("IsExpanded", typeof(bool), typeof(SidebarView),
-            new PropertyMetadata(false));
-
-    public bool IsExpanded
+    public partial class SidebarView : UserControl
     {
-      get { return (bool)GetValue(IsExpandedProperty); }
-      set { SetValue(IsExpandedProperty, value); }
-    }
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(SidebarView),
+                new PropertyMetadata(false));
 
-    // Evento para comunicar navegación a la ventana principal
-    public event Action<string> NavigationRequested;
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
 
-    public SidebarView()
-    {
-      InitializeComponent();
-    }
+        public event Action<string> NavigationRequested;
 
-    private void MenuButton_Click(object sender, RoutedEventArgs e)
-    {
-      // Obtener el tag del botón clickeado
-      var button = sender as RadioButton;
-      if (button?.Tag is string viewTag)
-      {
-        NavigationRequested?.Invoke(viewTag);
-      }
+        public SidebarView()
+        { 
+            InitializeComponent();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as RadioButton;
+            if (button?.Tag is string viewTag)
+            { 
+                NavigationRequested?.Invoke(viewTag);
+            }
+        }
+
+        private void ToggleExpansion_Click(object sender, RoutedEventArgs e)
+        {
+            IsExpanded = !IsExpanded;
+
+            if (IsExpanded)
+            {
+                ExpandSidebar();
+            }
+            else
+            {
+                CollapseSidebar();
+            }
+
+            ToggleExpansionButton.IsChecked = false;
+        }
+
+        private void ExpandSidebar()
+        {
+            var storyboard = (Storyboard)this.FindResource("ExpandSidebar");
+            storyboard.Begin(this);
+        }
+
+        private void CollapseSidebar()
+        {
+            var storyboard = (Storyboard)this.FindResource("CollapseSidebar");
+            storyboard.Begin(this);
+        }
     }
-  }
 }
