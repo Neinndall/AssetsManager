5ccf1152dcc16b3251e3d9e28a7cf4ba271dd599
Neinndall <danielpriego94@gmail.com>
2025-11-21 15:34:10 +0100

    added reportGenerationSettings and tweaks

--- AssetsManager/Utils/AppSettings.cs
+++ AssetsManager/Utils/AppSettings.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 using AssetsManager.Views.Models.Monitor;
+using AssetsManager.Views.Models.Shared;
 
 namespace AssetsManager.Utils
 {
