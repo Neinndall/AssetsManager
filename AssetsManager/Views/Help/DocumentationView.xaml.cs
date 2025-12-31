ddcefbd87dc28addd1f98ec7d5dd641f43f2b87a
Neinndall <danielpriego94@gmail.com>
2025-12-31 14:55:37 +0100

    feat: new documentation (main) help/tips

---
+++ AssetsManager/Views/Help/DocumentationView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AssetsManager.Views.Help
+{
+    public partial class DocumentationView : UserControl
+    {
+        public DocumentationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
