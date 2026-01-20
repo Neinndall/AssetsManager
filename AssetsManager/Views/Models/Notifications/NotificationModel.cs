d4728860ae4b1e560e4723494f613fc57053ed38
Neinndall <danielpriego94@gmail.com>
2026-01-21 00:15:00 +0100

    improvements system of notifications and other tweaks

--- AssetsManager/Views/Models/Notifications/NotificationModel.cs
+++ AssetsManager/Views/Models/Notifications/NotificationModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace AssetsManager.Views.Models.Notifications
 {
@@ -10,15 +12,30 @@ namespace AssetsManager.Views.Models.Notifications
         Error
     }
 
-    public class NotificationModel
+    public class NotificationModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isRead;
         public string Id { get; } = Guid.NewGuid().ToString();
         public string Title { get; set; }
         public string Message { get; set; }
         public NotificationType Type { get; set; }
         public DateTime Timestamp { get; set; }
-        public bool IsRead { get; set; }
         public Action OnClickAction { get; set; }
+
+        public bool IsRead
+        {
+            get => _isRead;
+            set
+            {
+                if (_isRead != value)
+                {
+                    _isRead = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
         public NotificationModel(string title, string message, NotificationType type)
         {
@@ -27,6 +44,11 @@ namespace AssetsManager.Views.Models.Notifications
             Type = type;
             Timestamp = DateTime.Now;
             IsRead = false;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
