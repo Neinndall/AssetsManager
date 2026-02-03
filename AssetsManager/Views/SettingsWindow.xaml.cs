4fe0e93faf30c52ecef3f3054d006e966b2a900d
Neinndall <danielpriego94@gmail.com>
2026-02-03 22:42:57 +0100

    feat: custom title bar

--- AssetsManager/Views/SettingsWindow.xaml.cs
+++ AssetsManager/Views/SettingsWindow.xaml.cs
@@ -112,6 +112,27 @@ namespace AssetsManager.Views
             SettingsChanged?.Invoke(this, new SettingsChangedEventArgs { WasResetToDefaults = false });
         }
 
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
 
     }
 }
