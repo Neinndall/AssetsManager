a1b26d9a85a67efcc141162ba7d2600d1bbd0fb5
Neinndall <66384195+Neinndall@users.noreply.github.com>
2026-05-06 15:51:15 +0200

    clear testing

--- AssetsManager/Utils/PerformanceBenchmark.cs
+++ AssetsManager/Utils/PerformanceBenchmark.cs
@@ -1,21 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using AssetsManager.Services.Explorer;
-using AssetsManager.Services.Hashes;
-using AssetsManager.Services.Core;
-using AssetsManager.Views.Models.Explorer;
-using AssetsManager.Views.Models.Wad;
-using AssetsManager.Utils.Framework;
 
 namespace AssetsManager.Utils
 {
     public static class PerformanceBenchmark
     {
-        // Performance Lab: Ready for new benchmarks.
+        /// <summary>
+        /// Placeholder for future performance tests.
+        /// </summary>
+        public static async Task<string> RunBenchmarkAsync()
+        {
+            await Task.Yield();
+            return "No benchmarks configured.";
+        }
     }
 }
