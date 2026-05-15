f84e14ec0056e477c87e5bf7e8029b7f6f3c328d
Neinndall <66384195+Neinndall@users.noreply.github.com>
2026-05-15 04:42:46 +0200

    some improvements to commit history window

--- AssetsManager/Services/Core/GitHubApiService.cs
+++ AssetsManager/Services/Core/GitHubApiService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Text.Json.Serialization;
 using System.Linq;
 using Serilog;
+using AssetsManager.Views.Models.Dialogs;
 
 namespace AssetsManager.Services.Core
 {
@@ -255,96 +256,4 @@ namespace AssetsManager.Services.Core
             }
         }
     }
-
-    #region Models
-
-    public class GitHubCommit
-    {
-        [JsonPropertyName("sha")]
-        public string Sha { get; set; }
-
-        [JsonPropertyName("commit")]
-        public CommitInfo Commit { get; set; }
-
-        [JsonPropertyName("author")]
-        public GitHubUser Author { get; set; }
-
-        [JsonPropertyName("html_url")]
-        public string HtmlUrl { get; set; }
-
-        // Local helper for UI to link with a build asset
-        public GitHubAsset DownloadableAsset { get; set; }
-        public GitHubAsset ParentBuildAsset { get; set; }
-        public string ParentBuildSha { get; set; }
-        public bool HasInheritedBuild => DownloadableAsset == null && ParentBuildAsset != null;
-        public bool IsLatest { get; set; }
-        public string ShortSha => Sha?.Substring(0, 7) ?? "";
-
-        // UI Helpers for segmented messages
-        public string MessageSummary => Commit?.Message?.Split('\n').FirstOrDefault()?.Trim() ?? string.Empty;
-        public string MessageBody
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(Commit?.Message)) return string.Empty;
-                var lines = Commit.Message.Split('\n');
-                if (lines.Length <= 1) return string.Empty;
-                return string.Join('\n', lines.Skip(1)).Trim();
-            }
-        }
-        public bool HasDescription => !string.IsNullOrEmpty(MessageBody);
-    }
-
-    public class CommitInfo
-    {
-        [JsonPropertyName("message")]
-        public string Message { get; set; }
-
-        [JsonPropertyName("author")]
-        public CommitAuthor Author { get; set; }
-    }
-
-    public class CommitAuthor
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("date")]
-        public DateTime Date { get; set; }
-    }
-
-    public class GitHubUser
-    {
-        [JsonPropertyName("login")]
-        public string Login { get; set; }
-
-        [JsonPropertyName("avatar_url")]
-        public string AvatarUrl { get; set; }
-    }
-
-    public class GitHubRelease
-    {
-        [JsonPropertyName("tag_name")]
-        public string TagName { get; set; }
-
-        [JsonPropertyName("assets")]
-        public List<GitHubAsset> Assets { get; set; }
-    }
-
-    public class GitHubAsset
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("browser_download_url")]
-        public string DownloadUrl { get; set; }
-
-        [JsonPropertyName("size")]
-        public long Size { get; set; }
-
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
-    }
-
-    #endregion
 }
--- AssetsManager/Views/Models/Dialogs/CommitHistoryModel.cs
+++ AssetsManager/Views/Models/Dialogs/CommitHistoryModel.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
 using AssetsManager.Services.Core;
 using AssetsManager.Utils.Framework;
 
@@ -47,4 +49,102 @@ namespace AssetsManager.Views.Models.Dialogs
         protected void OnPropertyChanged([CallerMemberName] string name = null)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
+
+    #region GitHub Models
+
+    public class GitHubCommit
+    {
+        [JsonPropertyName("sha")]
+        public string Sha { get; set; }
+
+        [JsonPropertyName("commit")]
+        public CommitInfo Commit { get; set; }
+
+        [JsonPropertyName("author")]
+        public GitHubUser Author { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public string HtmlUrl { get; set; }
+
+        // Local helper for UI to link with a build asset
+        public GitHubAsset DownloadableAsset { get; set; }
+        public GitHubAsset ParentBuildAsset { get; set; }
+        public string ParentBuildSha { get; set; }
+        public bool HasInheritedBuild => DownloadableAsset == null && ParentBuildAsset != null;
+        public bool IsLatest { get; set; }
+        public string ShortSha => Sha?.Substring(0, 7) ?? "";
+
+        // UI Helpers for segmented messages
+        public string MessageSummary => Commit?.Message?.Split('\n').FirstOrDefault()?.Trim() ?? string.Empty;
+        public string MessageBody
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Commit?.Message)) return string.Empty;
+                var lines = Commit.Message.Split('\n');
+                if (lines.Length <= 1) return string.Empty;
+
+                // Trim each line individually to remove indentation and filter empty lines
+                var trimmedLines = lines.Skip(1)
+                                        .Select(l => l.Trim())
+                                        .Where(l => !string.IsNullOrEmpty(l));
+
+                return string.Join(Environment.NewLine, trimmedLines);
+            }
+        }
+        public bool HasDescription => !string.IsNullOrEmpty(MessageBody);
+    }
+
+    public class CommitInfo
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("author")]
+        public CommitAuthor Author { get; set; }
+    }
+
+    public class CommitAuthor
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+    }
+
+    public class GitHubUser
+    {
+        [JsonPropertyName("login")]
+        public string Login { get; set; }
+
+        [JsonPropertyName("avatar_url")]
+        public string AvatarUrl { get; set; }
+    }
+
+    public class GitHubRelease
+    {
+        [JsonPropertyName("tag_name")]
+        public string TagName { get; set; }
+
+        [JsonPropertyName("assets")]
+        public List<GitHubAsset> Assets { get; set; }
+    }
+
+    public class GitHubAsset
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("browser_download_url")]
+        public string DownloadUrl { get; set; }
+
+        [JsonPropertyName("size")]
+        public long Size { get; set; }
+
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+
+    #endregion
 }
