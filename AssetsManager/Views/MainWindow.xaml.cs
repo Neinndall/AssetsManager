3a461eec10c98ea895b89dd3d087918844b1eef2
Neinndall <66384195+Neinndall@users.noreply.github.com>
2026-07-16 03:28:42 +0200

    improve comparison archive feedback

--- AssetsManager/Views/MainWindow.xaml.cs
+++ AssetsManager/Views/MainWindow.xaml.cs
@@ -267,6 +267,7 @@ namespace AssetsManager.Views
                 {
                     try
                     {
+                        _logService.Log("Saving comparison history...");
                         await _comparisonHistoryService.EnsureArchivedAsync(
                             serializableDiffs, oldPath, newPath, version, displayName);
                     }
